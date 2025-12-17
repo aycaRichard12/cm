0b0fb303b9ccdaeea5be3c1763e82947fb8197bc
aycaRichard12 <aycarichard7@gmail.com>
2025-12-17 16:11:40 -0400

    Depuracion archivos

--- src/composables/usuarios/vivasofts/menus/index_m.js
+++ src/composables/usuarios/vivasofts/menus/index_m.js
@@ -1,8 +1,4 @@
-import { menurichard50 } from './richard50'
-
 import { menutest75 } from './test75'
 export let menus = []
-
-menus.push({ richard50: menurichard50 })
 
 menus.push({ test75: menutest75 })
